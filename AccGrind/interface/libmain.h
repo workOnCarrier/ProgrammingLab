0a43e8519ce0b069bc41016418c141f83a4f1af0
Nitin Sharma <workOnCarrier@users.noreply.github.com>
2016-04-22 15:00:45 +0800

    Update libmain.h

--- AccGrind/interface/libmain.h
+++ AccGrind/interface/libmain.h
@@ -1,11 +1,21 @@
 #include "IInterpreter.h"
 #include <string>
 
+#ifdef WIN32
+#ifdef DLLEXPORT
+#define INTERFACE_EXPORT __declspec(dllexport)
+#else
+#define INTERFACE_EXPORT __declspec(dllimport)
+#endif
+#else
+#define INTERFACE_EXPORT 
+#endif
+
 extern "C"{
 #define PLUGINHANDLER "getPluginInputHandler" 
 #define DELETEPLUGIN "deletePluginInputHandler"
-AccGrind::IInterpreter<std::string>* getPluginInputHandler ( );
-void  deletePluginInputHandler ( void*);
+INTERFACE_EXPORT AccGrind::IInterpreter<std::string>* getPluginInputHandler ( );
+INTERFACE_EXPORT void  deletePluginInputHandler ( void*);
 
 }
 
