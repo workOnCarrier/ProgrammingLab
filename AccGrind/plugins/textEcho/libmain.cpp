cd1847fd769e60641fa8b8445ccd19ddd61aabf5
Nitin Sharma <workOnCarrier@users.noreply.github.com>
2016-04-22 15:02:45 +0800

    Update libmain.cpp

--- AccGrind/plugins/textEcho/libmain.cpp
+++ AccGrind/plugins/textEcho/libmain.cpp
@@ -1,3 +1,4 @@
+#define DLLEXPORT
 #include "libmain.h"
 #include <string>
 #include "textInterpreter.h"
@@ -7,10 +8,10 @@ using namespace AccGrindPlugin;
 #pragma message ("exporting as C api : getPluginInputHandler ")
 
 extern "C"{
-    AccGrind::IInterpreter<std::string>* getPluginInputHandler () {
+    INTERFACE_EXPORT AccGrind::IInterpreter<std::string>* getPluginInputHandler () {
         return new TextInterpreter ();
     }
-    void  deletePluginInputHandler ( void* ptr)
+    INTERFACE_EXPORT void  deletePluginInputHandler ( void* ptr)
     {
         AccGrind::IInterpreter<std::string>* actualPtr = reinterpret_cast<AccGrind::IInterpreter<std::string>*> (ptr); 
         delete(actualPtr);
