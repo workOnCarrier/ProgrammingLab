7be62a00b26b6e0f57d3cd50289122b850632319
workOnCarrier <workonideas@gmail.com>
2016-08-14 17:23:36 +0800

    first working version of test programs

---
+++ modernCpp/ModernC++Tdd/mycode/c2/1/first.cpp
@@ -0,0 +1,11 @@
+
+class Soundex{
+public:
+    Soundex(){}
+
+};
+// #include "gmock/gmock.h"
+#include "gtest/gtest.h"
+TEST ( SoundexEncoding, RetainSoleLetterOfOneLetterWord ){
+    Soundex soundex;
+}
