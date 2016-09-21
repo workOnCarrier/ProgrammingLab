c848f625b54db634d11cdee06c5c6e8e8f4aae0a
Nitin Sharma <nitinsharma.pec@gmail.com>
2016-09-21 22:30:16 +0800

    adding move learnings

---
+++ modernCpp/train/trying_move.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void try_const_rvalue ();
+
+int main (int argc, char** argv) {
+    try_const_rvalue ();
+    return 0;
+}
+
+void try_const_rvalue () {
+    // try const rvalues as per the tutorials
+}
