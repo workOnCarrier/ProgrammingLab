25fdeda897e04f682fa2752ba67ce38a7a591b14
Nitin Sharma <nitinsharma.pec@gmail.com>
2016-09-22 13:12:21 +0800

    adding temporary version of assert usage

--- modernCpp/train/trying_assert.cpp
+++ modernCpp/train/trying_assert.cpp
@@ -13,10 +13,26 @@ int check_static_assert ( int a, int b ) {
 }
 
 // in templates for validation
+template <int dimention>
+class template_assert {
+public:
+    template_assert (){
+        static_assert ( dimention < 4 , "cannot handle dimention more than 3" );
+    }
+	void display () {
+	}
+private:
+    int values[dimention] ;
+};
 
 
 
 int main ( int argc , char** argv ) {
     check_static_assert(4,5);
+
+	template_assert<3>		obj_template_assert;
+	// should not compile
+	// template_assert<4>		obj_template_assert_not_working;
+
     return 0;
 }
