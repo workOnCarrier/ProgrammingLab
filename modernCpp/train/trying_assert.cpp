7a8f23ec6b7a2edb917906ed81576d51d6bd38a2
Nitin Sharma <nitinsharma.pec@gmail.com>
2016-09-27 12:06:03 +0800

    add more code for trying out asserts

--- modernCpp/train/trying_assert.cpp
+++ modernCpp/train/trying_assert.cpp
@@ -16,8 +16,8 @@ int check_static_assert ( int a, int b ) {
 template <int dimention>
 class template_assert {
 public:
+    static_assert ( dimention < 4 , "cannot handle dimention more than 3" );
     template_assert (){
-        static_assert ( dimention < 4 , "cannot handle dimention more than 3" );
     }
 	void display () {
 	}
@@ -25,14 +25,29 @@ private:
     int values[dimention] ;
 };
 
+struct base {
+public:
+    virtual ~base(){}
+};
+
+template <typename T>
+class my_mixin : public T {
+    static_assert ( has_virtual_destructor<T>::value, "The destructor has to be virtual " );
+    public:
+	my_mixin(){}
+};
 
 
 int main ( int argc , char** argv ) {
     check_static_assert(4,5);
 
 	template_assert<3>		obj_template_assert;
-	// should not compile
+	// will not compile
 	// template_assert<4>		obj_template_assert_not_working;
 
+
+	my_mixin <base>			obj_my_mixin;
+	// will not compile
+	// my_mixin <int>			obj_my_mixin_invalid;
     return 0;
 }
